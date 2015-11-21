d417d9ceefb3cb670e32ac948c7b59d66fc1ce68
Rifat Hasan <rifathsn@Rifats-MacBook-Pro.local>
2015-11-21 02:58:14 -0500

    Added map functionality in event page and set a url in dashboard to navigate to event

--- public/js/event.js
+++ public/js/event.js
@@ -7,7 +7,7 @@ angular.module('angularApp')
         var requestDone=$q.defer();
 
         var eventData = webservice.getEventDatafromApi($routeParams.id);
-
+        console.log(eventData);
         eventData.then(
             function (payload) {
                 // load data from api call to required fields
@@ -109,10 +109,16 @@ angular.module('angularApp')
             function initialize() {
                 var mapProp = {
                     center:new google.maps.LatLng($scope.eventLocationLat,$scope.eventLocationLng),
-                    zoom:5,
+                    zoom:10,
                     mapTypeId:google.maps.MapTypeId.ROADMAP
                 };
+
                 var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+                var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng($scope.eventLocationLat,$scope.eventLocationLng),
+                    map: map
+                });    
             }
             google.maps.event.addDomListener(window, 'load', initialize);
         });
